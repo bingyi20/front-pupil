7c7b886d551868fe2b5b7396111116bba7612831
bingyi20 <yibangyu@gmail.com>
2021-07-19 23:04:59 +0800

    feat: 模拟async await开发完成

---
+++ PromiseA+/asyncFunc.js
@@ -0,0 +1,44 @@
+
+
+function longTimeTask(value, timeout) {
+    return new Promise((resolve) => {
+        setTimeout(()=>{
+            resolve(value)
+        }, timeout)
+    })
+}
+
+/**
+ * 模拟ascnc await
+ * @param {Function} generator 生成器函数
+ */
+function asyncFunc(generator) {
+    const iterator = generator()
+
+    const next = (res) => {
+        const {done, value} = iterator.next(res)
+        if(done) {
+            return  value
+        }
+
+        Promise.resolve(value).then(res => {
+            next(res)
+        })
+    }
+
+    next()
+
+}
+
+function *generator() {
+    const value1 = yield longTimeTask(111, 1000)
+    console.log(value1)
+    const value2 = yield longTimeTask(222, 2000)
+    console.log(value2)
+    const value3 = yield longTimeTask(333, 500)
+    console.log(value3)
+    console.log(value1, value2, value3)
+}
+
+asyncFunc(generator)
+
